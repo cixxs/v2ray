b8f219e29b462e90ed321196a077bfd12700eb07
cixxs <80149990+cixxs@users.noreply.github.com>
2021-03-13 13:55:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -173,7 +173,7 @@ v2ray_config() {
 		echo "备注1: 含有 [dynamicPort] 的即启用动态端口.."
 		echo "备注2: [utp | srtp | wechat-video | dtls | wireguard] 分别伪装成 [BT下载 | 视频通话 | 微信视频通话 | DTLS 1.2 数据包 | WireGuard 数据包]"
 		echo
-		v2ray_transport=1
+		v2ray_transport=12
 		[ -z "$v2ray_transport" ] && v2ray_transport=1
 		case $v2ray_transport in
 		[1-9] | [1-2][0-9] | 3[0-3])
