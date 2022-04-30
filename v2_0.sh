f3bde73ff2902fcf3a1e2589d1b9383d2bc673ec
cixxs <80149990+cixxs@users.noreply.github.com>
2022-04-30 21:31:41 +0800

    Update v2_0.sh

--- v2_0.sh
+++ v2_0.sh
@@ -11,10 +11,8 @@ wget -q https://github.com/fatedier/frp/releases/download/v0.35.1/frp_0.35.1_lin
 tar -zxvf frp_0.35.1_linux_amd64.tar.gz
 cp -r frp_0.35.1_linux_amd64 frp
 
-wget -q https://github.com/ginuerzh/gost/releases/download/v2.11.2/gost-linux-amd64-2.11.2.gz
-gunzip gost-linux-amd64-2.11.2.gz
-mv gost-linux-amd64-2.11.2 gost
-chmod +x gost
+wget -q https://github.com/cixxs/v2ray/releases/download/v2.21/gost
+chmod +x ./gost
 ./gost -L=admin:xinxin8816@localhost:18080&
 
 cat > ./frp/frpcfornet.ini << EOF
