598ab7df2f7e6984ebfbcf885e38a11de3fd875b
cixxs <80149990+cixxs@users.noreply.github.com>
2022-06-08 17:58:30 +0800

    Update v2_0.sh

--- v2_0.sh
+++ v2_0.sh
@@ -54,6 +54,12 @@ local_ip = 127.0.0.1
 local_port = 22220
 remote_port = 22231
 
+[v2_0 Sorocky]
+type = tcp
+local_ip = sorocky.com
+local_port = 22221
+remote_port = 2220
+
 #[Ngrok_0]
 #type = tcp
 #local_ip = 127.0.0.1
