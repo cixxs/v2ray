3f11ae0b95f6ff80390a67bb21ed07caffa3bc47
Zealer Chan <8501725+xinxin8816@users.noreply.github.com>
2023-03-23 15:21:08 +0800

    Update v2_0.sh

--- v2_0.sh
+++ v2_0.sh
@@ -328,6 +328,17 @@ cat > /root/workspace/code/config.json << EOF
 		"settings": {
 			"domainStrategy": "UseIP"
 		}
+	}, {
+		"tag": "netmusic_out",
+		"protocol": "http",
+		"settings": {
+			  "servers": [
+				{
+				  "address": "sg.sorocky.com",
+				  "port": 8080
+				}
+			  ]
+		}
 	}],
 	"routing": {
 		"domainStrategy": "IPIfNonMatch",
@@ -344,6 +355,10 @@ cat > /root/workspace/code/config.json << EOF
 				"outboundTag": "IPv4_out",
 				"ip": ["0.0.0.0/0"]
 			}, {
+				"type": "field",
+				"outboundTag": "netmusic_out",
+				"domain": ["music.163.com","music.126.net"]
+			},  {
 				"type": "field",
 				"outboundTag": "IPv4_out",
 				"ip": ["::/0"]
