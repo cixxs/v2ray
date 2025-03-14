b92223cc79d74f43b96055c3487fa552537bab90
cixxs <80149990+cixxs@users.noreply.github.com>
2025-03-14 15:27:24 +0800

    Update v2_1.sh

--- v2_1.sh
+++ v2_1.sh
@@ -428,7 +428,7 @@ cat > /root/workspace/code/config.json << EOF
         "rules":[
             {
                 "type":"field",
-                "outboundTag":"enterprise",
+                "outboundTag":"IPv4_out",
                 "domain":[
                     "geosite:youtube"
                 ]
